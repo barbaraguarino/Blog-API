b8c79492bedf6113a66599f70ad74bd970ee7a2a
Barbara <40583024+barbaraguarino@users.noreply.github.com>
2026-04-03 17:52:06 -0300

    Criação básica do repositório.

---
+++ src/main/java/br/com/grifo/modules/user/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package br.com.grifo.modules.user.repositories;
+
+import br.com.grifo.modules.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+}
