cd57e1f6fd17ed22380d8a09a50a709728a02103
Barbara <40583024+barbaraguarino@users.noreply.github.com>
2026-04-03 17:52:24 -0300

    Criação do DTO de cadastrar conta.

---
+++ src/main/java/br/com/grifo/modules/user/dtos/UserRegistrationDTO.java
@@ -0,0 +1,25 @@
+package br.com.grifo.modules.user.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserRegistrationDTO(
+        @NotBlank(message = "{error.user.name.not_blank}")
+        @Size(max = 150, message = "{error.user.name.size}")
+        String name,
+
+        @Email(message = "{error.user.email.invalid}")
+        @NotBlank(message = "{error.user.email.not_blank}")
+        @Size(max = 150, message = "{error.user.email.size}")
+        String email,
+
+        @NotBlank(message = "{error.user.password.not_blank}")
+        @Size(min = 8, message = "{error.user.password.size}")
+        @Pattern(
+                regexp = "^(?=(?:.*[A-Z]){2})(?=(?:.*[a-z]){2})(?=(?:.*\\d){2})(?=.*[\\W_]).*$",
+                message = "{error.user.password.complexity}"
+        )
+        String password
+) {}
