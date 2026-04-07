8b259fc2871efc54d6f1486c25042108e3d48333
Barbara <40583024+barbaraguarino@users.noreply.github.com>
2026-04-07 12:06:48 -0300

    Renomeando e adicionando as funcionalidades

--- src/main/java/br/com/grifo/modules/user/controllers/UserRegistrationController.java
+++ src/main/java/br/com/grifo/modules/user/controllers/UserRegistrationController.java
@@ -1,8 +1,9 @@
 package br.com.grifo.modules.user.controllers;
 
+import br.com.grifo.modules.user.dtos.GoogleTokenDTO;
 import br.com.grifo.modules.user.dtos.UserRegistrationDTO;
 import br.com.grifo.modules.user.dtos.UserResponseDTO;
-import br.com.grifo.modules.user.services.UserService;
+import br.com.grifo.modules.user.services.UserRegistrationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,14 +14,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/register")
 @RequiredArgsConstructor
-public class UserController {
+public class UserRegistrationController {
 
-    private final UserService userService;
+    private final UserRegistrationService userRegistrationService;
 
-    @PostMapping("/register")
+    @PostMapping()
     public ResponseEntity<UserResponseDTO> register(@RequestBody @Valid UserRegistrationDTO dto){
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(dto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRegistrationService.registerUser(dto));
+    }
+
+    @PostMapping("/google")
+    public ResponseEntity<UserResponseDTO> registerWithGoogle(@RequestBody @Valid GoogleTokenDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRegistrationService.registerWithGoogle(dto));
     }
 }
