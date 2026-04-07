968ec9b4666d51e4a695ef63bda5758303fadb16
Barbara <40583024+barbaraguarino@users.noreply.github.com>
2026-04-07 12:43:03 -0300

    Limpando

--- src/main/java/br/com/grifo/modules/user/domain/User.java
+++ src/main/java/br/com/grifo/modules/user/domain/User.java
@@ -51,6 +51,7 @@ public class User implements UserDetails {
     }
 
     @Override
+    @NonNull
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(this.role.getRole()));
     }
@@ -61,13 +62,9 @@ public class User implements UserDetails {
     }
 
     @Override
+    @NonNull
     public String getUsername() {
         return this.email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
     }
 
     @Override
