2f562503d37615f2324fc2829061ded0b1d800e5
Barbara <40583024+barbaraguarino@users.noreply.github.com>
2026-06-28 17:10:40 -0300

    Transformando a classe ApiErrorResponse.java em ErrorResponseDTO.java

---
+++ src/main/java/br/com/blog/core/exceptions/ErrorResponseDTO.java
@@ -0,0 +1,45 @@
+package br.com.blog.core.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ErrorResponseDTO (
+
+    LocalDateTime timestamp,
+    Integer status,
+    String error,
+    String message,
+    String path,
+    Map<String, String> validationErrors
+){
+    public static ErrorResponseDTO create(Integer status,
+                                          String error,
+                                          String message,
+                                          String path,
+                                          Map<String, String> validationErrors) {
+        return new ErrorResponseDTO(
+                LocalDateTime.now(),
+                status,
+                error,
+                message,
+                path,
+                validationErrors);
+    }
+
+    public static ErrorResponseDTO create(Integer status,
+                                          String error,
+                                          String message,
+                                          String path) {
+        return new ErrorResponseDTO(
+                LocalDateTime.now(),
+                status,
+                error,
+                message,
+                path,
+                null
+        );
+    }
+}
